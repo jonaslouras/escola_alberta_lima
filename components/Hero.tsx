c5f2bd6cd247f9a7a5bb5fbe6eef2869eeebf2b7
jonaslouras <40373@alunos.upt.pt>
2026-02-21 04:51:28 +0000

    hero fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero: React.FC<HeroProps> = ({ scrollToSection }) => {
       {/* Background Image with Overlay */}
       <div className="absolute inset-0 z-0">
         <img
-          src="/images/avo.jpg"
+          src="/images/hero.png"
           alt="Interior da Escola de Música Alberta Lima em Matosinhos"
           className="w-full h-full object-cover"
         />
